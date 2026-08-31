d2ae546db1b1f92ddf10aaa88cd1524a18284a30
Martin <martin@its.mu>
2026-08-31 18:38:32 +0400

    chore(release): 16.0.2

--- crema/__init__.py
+++ crema/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "16.0.1"
+__version__ = "16.0.2"
 
 from crema.api import ask, ask_json, configure, extract, health, is_configured, ocr, transcribe, transform
 from crema.exceptions import CremaBlockedError, CremaBudgetError, CremaConfigError
